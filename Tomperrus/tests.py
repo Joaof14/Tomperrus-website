37ca40f512bd6c9b0328c95031b4d54d5ea7a831
joaoF14 <joaopedro140003@gmail.com>
2021-11-22 15:58:06 -0300

    some web tests in construction

--- Tomperrus/tests.py
+++ Tomperrus/tests.py
@@ -1,10 +1,23 @@
 from django.http import response
 from django.test import Client, TestCase, client
-
 from Tomperrus.views import products
 from .models import Product, Flavour, Image
+import unittest
+from selenium import webdriver
+import os
+import pathlib
+#Create your tests here.
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
 
-# Create your tests here.
+driver = webdriver.Chrome()
+class WebPageLayoutTests(unittest.TestCase):
+    driver.get(file_uri("layout.html"))
+    def test_title(self):
+        self.assertEqual(driver.title, "Tomperrus")
+    def test_headimg(self):
+        self.assertEqual(driver.find_element_by_id("logo").src, "/static/staticMedia/perfil.jpg")
+
 class ProductTestCase(TestCase):
     def setUp(self):
         p1 =  Product.objects.create(Name = "produto", Price = 12, ImageFile = "media/images/chimichurri.jpeg", 
