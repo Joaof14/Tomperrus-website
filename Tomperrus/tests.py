3bb0905b4ca9fb869134d66040680abdd5294167
joaoF14 <joaopedro140003@gmail.com>
2022-01-24 15:57:49 -0300

    tests

--- Tomperrus/tests.py
+++ Tomperrus/tests.py
@@ -7,16 +7,6 @@ from selenium import webdriver
 import os
 import pathlib
 #Create your tests here.
-def file_uri(filename):
-    return pathlib.Path(os.path.abspath(filename)).as_uri()
-
-driver = webdriver.Chrome()
-class WebPageLayoutTests(unittest.TestCase):
-    driver.get(file_uri("layout.html"))
-    def test_title(self):
-        self.assertEqual(driver.title, "Tomperrus")
-    def test_headimg(self):
-        self.assertEqual(driver.find_element_by_id("logo").src, "/static/staticMedia/perfil.jpg")
 
 class ProductTestCase(TestCase):
     def setUp(self):
