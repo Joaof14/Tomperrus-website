18d7fcbfa7a2b00c124fbb87c92b22dce16afb8c
joaoF14 <joaopedro140003@gmail.com>
2022-03-03 11:13:47 -0300

    Tomperrus

--- Tomperrus/views.py
+++ Tomperrus/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Tomperrus.models import Product, Flavour, Image
+from django.http import Http404
 
 # Create your views here.
 #products = [['Chimichurri tradicional', 'pão de alho.jpg', 'descrição'],['Chimichurri sabor bacon', 'pão de alho.jpg', 'descrição'],['Chimichurri sabor apimentado', 'pão de alho.jpg', 'descrição'], ['Farofa de soja', 'farofa1.jpg', 'descrição'],['Pão de alho',  'imagem', 'pão de alho.jpg']]
@@ -8,6 +9,10 @@ def index(request):
         "products": Product.objects.all()
     })
 def products(request, Product_Name):
+    try:
+        Pr = Product.objects.get(Name = Product_Name)
+    except Product.DoesNotExist:
+        return render(request, "tomperrus/error.html")
     product = Product.objects.get(Name = Product_Name)
     flavours = Flavour.objects.filter(ProductN = product)
     images = Image.objects.filter(ProductN = product)
