dbba9ad9efdd207b1f2885ff6163cc929bada858
joaoF14 <joaopedro140003@gmail.com>
2022-03-07 11:12:38 -0300

    "changed how the error would be displayed etc"

--- Tomperrus/views.py
+++ Tomperrus/views.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from django.shortcuts import render
 from Tomperrus.models import Product, Flavour, Image
 from django.http import Http404
@@ -12,7 +13,8 @@ def products(request, Product_Name):
     try:
         Pr = Product.objects.get(Name = Product_Name)
     except Product.DoesNotExist:
-        return render(request, "tomperrus/error.html")
+        return render(request, "tomperrus/index.html", 
+        {"error" : "Produto não encontrado, volte a pagina inicial"})
     product = Product.objects.get(Name = Product_Name)
     flavours = Flavour.objects.filter(ProductN = product)
     images = Image.objects.filter(ProductN = product)
