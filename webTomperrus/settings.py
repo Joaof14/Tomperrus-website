bf506c8d61a8224afc8ee6e71806eb0472001bed
joaoF14 <joaopedro140003@gmail.com>
2022-05-18 09:26:36 -0300

    database change

--- webTomperrus/settings.py
+++ webTomperrus/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'webTomperrus.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TomperrusDB',
-        'USER': 'Joaof14',
-        'PASSWORD': 'jp123fernandes',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    'default': { 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
